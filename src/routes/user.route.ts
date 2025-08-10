378bc3b0e3c66eebbf7326e491c1e8668aa3040c
jiaul.jibon <jiaul.jibon@strativ.se>
2025-08-10 21:26:49 +0600

    refactor: add authentication middleware to user routes and serialize user responses

--- src/routes/user.route.ts
+++ src/routes/user.route.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 import { UserService } from '../service/user.service';
+import { requireAuthMiddleware } from '@jiaulislam.dev/common.ticketing.dev';
 const router = express.Router();
 
 const userService = new UserService();
@@ -11,12 +12,12 @@ router.get('/', async (_, res) => {
   res.json(serializedUsers);
 });
 
-router.post('/', async (req, res) => {
+router.post('/', requireAuthMiddleware, async (req, res) => {
   const newUser = await userService.createUser(req.body);
   res.status(201).json(userService.serializeUser(newUser));
 });
 
-router.put('/:id', async (req, res) => {
+router.put('/:id', requireAuthMiddleware, async (req, res) => {
   const userId = req.params.id;
   const updatedUser = await userService.updateUser(Number(userId), req.body);
   res.json(updatedUser);
@@ -25,11 +26,12 @@ router.put('/:id', async (req, res) => {
 router.get('/:id', async (req, res) => {
   const userId = req.params.id;
   const user = await userService.getUserById(Number(userId));
-  res.json(user);
+  const serializedUser = userService.serializeUser(user!)
+  res.json(serializedUser);
 });
 
-router.get('/profile', async (req, res) => {
-  const userId = req.currentUser?.id; // Assuming you have user ID in req.user
+router.get('/profile', requireAuthMiddleware, async (req, res) => {
+  const userId = req.currentUser!.id; // Assuming you have user ID in req.user
   const user = await userService.getUserById(Number(userId));
   res.json(user);
 });
